9906aad99f391985e8fad0fafcfff02aae82176f
iwandewit <iwan.de.wit@hva.nl>
2025-09-18 09:13:26 +0200

    Add reservation service

--- backend/src/main/java/com/iwan/bootapp/backend/service/ReservationService.java
+++ backend/src/main/java/com/iwan/bootapp/backend/service/ReservationService.java
@@ -1,5 +1,23 @@
 package com.iwan.bootapp.backend.service;
 
+import java.time.LocalDate;
+
+import org.springframework.stereotype.Service;
+import com.iwan.bootapp.backend.repository.ReservationRepository;
+import com.iwan.bootapp.backend.model.Reservation;
+import com.iwan.bootapp.backend.model.User;
+
+@Service
 public class ReservationService {
+
+    private final ReservationRepository reservationRepository;
+
+    public ReservationService(ReservationRepository reservationRepository) {
+        this.reservationRepository = reservationRepository;
+    }
     
+    public Reservation createReservation (User user, LocalDate startDate, LocalDate endDate) {
+    Reservation reservation = new Reservation(startDate, endDate, user);
+    return reservationRepository.save(reservation);
+    }
 }
